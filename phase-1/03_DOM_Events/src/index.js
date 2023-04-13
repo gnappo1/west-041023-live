563aecbd4c912edc0a0a824c74a2de84282ba0b2
Matteo Piccini <gnappo.1@live.it>
2023-04-13 13:32:07 -0700

    starter code

--- phase-1/03_DOM_Events/src/index.js
+++ phase-1/03_DOM_Events/src/index.js
@@ -8,7 +8,6 @@
 ////////////////////////////////////////////////////////////////
 // Yesterday's Code
 ////////////////////////////////////////////////////////////////
-document.addEventListener('DOMContentLoaded', e => {
 
 console.log(bookStore);
 
@@ -82,77 +81,5 @@ bookStore.inventory.forEach(bookObj => renderBookAsHTML(bookObj))
 // bookStore.inventory.forEach(renderBook) this line leverages JS magic BUT IT'S IDENTICAL TO THE ONE ABOVE
 
 ////////////////////////////////////////////////////////////////
-// Today's Code
 // Event Listeners/Handlers (Behavior => Data => Display)
 ////////////////////////////////////////////////////////////////
-// domNodeElement.addEventListener(theEventInStringformat, callbackFunctionThatDesidesWhatToDo)
-    const toggleBtn = document.querySelector("#toggleForm")
-    const form = document.querySelector("#book-form")
-
-    //! Pattern 1: create the function somewhere to promote reusability
-    //! then pass the function as a callback to addEventListener
-    // const handleClick = e => {
-    //     // debugger
-    //     const isVisible = form.classList.toggle("collapsed")
-    //     if (isVisible) {
-    //         toggleBtn.innerText = "Add Book"
-    //     } else {
-    //         toggleBtn.innerText = "Hide Form"
-    //     }
-        
-    // }
-    // toggleBtn.addEventListener("click", handleClick) //! DO NOT TYPE () after the function name or it would not be a callBACK anymore. It would be invoked immediately and not when the event actually triggers.
-
-    //! Pattern 2: create the callback function in-place, make it anonymous, and IF YOU WANT use an arrow function for readability.
-    toggleBtn.addEventListener("click", () => {
-        //! If the event object is not needed, you can omit it like above
-        // debugger
-        //! classList returns an array-like structure that shows all the classnames available on the element specified
-        const isVisible = form.classList.toggle("collapsed")
-        //! toggle returns a boolean based on whether the classname is actually present on the element or not
-        if (isVisible) {
-            toggleBtn.innerText = "Add Book"
-        } else {
-            toggleBtn.innerText = "Hide Form"
-        }
-    })
-
-    form.addEventListener("submit", e => {
-        e.preventDefault() // prevent the submit event from refreshing the page
-        //! You can extract the value of inputs inside a form using . notation and the input's name/id properties if in camel-case notation
-        //! You can extract the value of inputs inside a form using [] notation and the input's name/id properties independently of the notation
-        const title = e.target.title.value
-        const author = e.target.author.value
-        const price = e.target.price.value
-        const imageUrl = e.target.imageUrl.value
-        const inventory = e.target.inventory.value
-        //! Package the data into an object because the renderBook functions expect an object as argument
-        const bookObj = {
-            title,
-            author,
-            imageUrl,
-            price,
-            inventory
-        }
-        bookStore.inventory.push(bookObj) //* desperate attempt to persist the book across page rerendering! The correct way will be showcased next week!
-        renderBookAsHTML(bookObj) //* put new book on the page
-        e.target.reset() //* empty each input in the form
-        form.classList.toggle("collapsed") //* toggle visibility
-
-    })
-
-    window.addEventListener('keydown', e => {
-        if (e.key === "Escape") {
-            
-            //! form.classList.add("collapsed") could be enough but let's use a conditional statement OR ternary expression to make sure 
-            !form.classList.contains("collapsed") ? form.classList.add("collapsed") : null
-
-            //! the above is equivalent to
-            // if (!form.classList.contains("collapsed")) {
-            //     form.classList.add("collapsed")
-            // }
-        }
-    })
-    
-    
-})
