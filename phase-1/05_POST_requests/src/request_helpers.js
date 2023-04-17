3c6d42780cbdaf0496f7214fefbcbe3090af7674
Matteo Piccini <gnappo.1@live.it>
2023-04-17 10:36:12 -0700

    pre=-lecture code

---
+++ phase-1/05_POST_requests/src/request_helpers.js
@@ -0,0 +1,27 @@
+function getJSON(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw (response.statusText)
+      }
+    })
+}
+
+function postJSON(url, data) {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw (response.statusText)
+      }
+    })
+}
