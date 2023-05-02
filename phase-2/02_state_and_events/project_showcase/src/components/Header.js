374ea7babe985be319ddfce136e3e261fadcfc07
Matteo Piccini <gnappo.1@live.it>
2023-05-02 09:27:01 -0700

    pre-lecture code

---
+++ phase-2/02_state_and_events/project_showcase/src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <span className="logo">{"//"}</span>
+        Project Showcase
+      </h1>
+      <button>Dark Mode</button>
+    </header>
+  );
+}
+
+export default Header;
