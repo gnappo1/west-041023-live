a6b8d5473181fdb61ca710e20078e34e841bcc7f
Matteo Piccini <gnappo.1@live.it>
2023-05-11 17:50:16 -0700

    Set up starter code for lecture

---
+++ phase-3/01_python_fundamentals/README.md
@@ -0,0 +1,13 @@
+# Python Fundamentals
+
+## Learning Goals
+
+- Introduce Python and outline phase trajectory 
+- Demonstrate Python package management with `pip`
+- Demonstrate debugging in Python with `shell`, `print`, and `ipdb`
+- Review Python data types (`str`, `int`, `float`, `bool`, `None`)
+- Demonstrate Python conditionals and control flow
+- Demonstrate Python functions
+- Review Python variable scope and the `global` keyword
+- Review Python error messages and exceptions 
+- Demonstrate handling errors with `try:` and `except:`
---
+++ phase-3/01_python_fundamentals/lib/app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# 📚 Review:
+    # Python Environment Setup
+	# Python Debugging Tools
+	# Python Data Types 
+
+# 🚨 To enable ipdb debugging, first import "ipdb"
+import ipdb
+
+#* Initial Discussion
+
+# 0. Style Guide (visit: https://peps.python.org/pep-0008/#code-lay-out)
+# 1. Data Types
+# 2. Variable Naming Conventions (visit https://peps.python.org/pep-0008/#naming-conventions)
+# 3. String Interpolation
+
+pet_mood = "Hungry!"
+pet_name = "Rose"
+
+#TODO 1. ✅ Create a condition to check a pet's mood
+    # If "pet_mood" is "Hungry!", "Rose needs to be fed."
+    # If "pet_mood" is "Rowdy!", "Rose needs a walk."
+    # In all other cases, "Rose is all good."
+
+    # Note => Feel free to set your own values for "pet_mood" to view various outputs.
+
+#TODO 2. ✅ Create a ternary operator using "pet_mood" as a condition:
+    # If pet_food is "Hungry!" => "Rose needs to be fed."
+    # In all other cases => "Rose is all good."
+
+#TODO 3. ✅ Create a function (say_hello) that returns the string "Hello, world!"
+    # Test invocation of "say_hello" in ipdb using "say_hello()"
+    # say_hello() => "Hello, world!"
+
+#TODO 4. ✅ Create a function (pet_greeting) that will return a string with interpolated pet's name
+    # Test invocation of "pet_greeting" in ipdb using "pet_greeting()"
+    # pet_greeting("Rose") => "Rose says hello!"
+    # pet_greeting("Spot") => "Spot says hello!"
+
+#TODO 5. ✅ Move conditional logic from Deliverable 1 into a function (pet_status) so that we may use it with different pets / moods
+    # Test invocation of "pet_status" in ipdb using "pet_status(pet_name, pet_mood)"
+    # pet_status("Rose", "Hungry!") => "Rose needs to be fed."
+    # pet_greeting("Spot", "Rowdy!") => "Spot needs a walk."
+    # pet_greeting("Bud", "Relaxed") => "Bud is all good."
+    
+    # Take a moment to note that "pet_name" and "pet_mood" parameters are within Local Scope and take priority over "pet_name" and "pet_mood"
+    # in Global Scope.
+
+#TODO 6. ✅ Create a function (pet_birthday) that will increment a pet's age up by 1. Use try / except to handle errors. 
+    # If our function is given an incorrect datatype, it should handle the TypeError exception and alert the user
+    # pet_birthday(10) => "Happy Birthday! Your pet is now 11."
+    # pet_birthday("oops") => "Type Error Occurred"
+
+    # Note => To view more common Python exceptions, visit https://docs.python.org/3/library/exceptions.html
+
+# 🚨 To create an ipdb breakpoint, comment / uncomment line below:
+# ipdb.set_trace()
+
+
